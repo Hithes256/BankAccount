8034f4214d5f1e69a35015dbd9d390228b2d1ce6
Hithes256 <hitheshkumar246@gmail.com>
2026-01-23 08:54:48 +0530

    Update initial balance and deposit amount in main

--- BankService.java
+++ BankService.java
@@ -33,16 +33,16 @@ public class BankService {
     // The main entry point
     public static void main(String[] args) {
         // Create an instance of the BankAccount class
-        BankAccount acc = new BankAccount(5000);
+        BankAccount acc = new BankAccount(8000);
         
         System.out.println("Initial Balance: " + acc.getBalance());
 
         // Perform operations
-        acc.deposit(700);
+        acc.deposit(500);
         System.out.println("Balance after deposit of 500: " + acc.getBalance());
 
         acc.withdraw(100);
-        System.out.println("Balance after withdrawal of 300: " + acc.getBalance());
+        System.out.println("Balance after withdrawal of 100: " + acc.getBalance());
 
         // Print final result
         System.out.println("Final Balance: " + acc.getBalance());
