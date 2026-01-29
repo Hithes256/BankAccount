d2ea1c1353a11c78dc5c335deb9e07aa1bf1a85b
Hithes256 <hitheshkumar246@gmail.com>
2026-01-29 17:15:10 +0530

    Implement basic banking operations in Bank.java

---
+++ Bank.java
@@ -0,0 +1,22 @@
+public class Bank{
+  public static void main(String[] args){
+    double balance=1000;
+    double depositAmt=500;
+    double withdrawAmt=300;
+
+    System.out.println("Initial Balance: "+balance);
+
+    balance+=depositAmt;
+    System.out.println("Balance after Depositing "+depositAmt+" : "+balance);
+
+    if(withdraw > balance){
+      System.out.println("Insufficient Balance! Can't withdraw");
+    }
+    else{
+      balance-=withdraw;
+      System.out.println("Balance after withdrawing "+withdrawAmt+" : "+balance);
+    }
+
+    System.out.println("Final Balance: "+balance);
+  }
+}
