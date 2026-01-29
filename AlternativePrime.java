3ef1de35bd2523e09d6c77cc950ddcc0528f33a3
Hithes256 <hitheshkumar246@gmail.com>
2026-01-29 17:55:22 +0530

    Update AlternativePrime.java

--- AlternativePrime.java
+++ AlternativePrime.java
@@ -8,13 +8,13 @@ public class AlternativePrime{
           isPrime=false;
           break;
         }
-        if(isPrime){
-          count++;
-          if(count%2==1){
-            System.out.println(num+" ");
-          }
+      if(isPrime){
+        count++;
+        if(count%2==1){
+          System.out.print(num+" ");
         }
       }
     }
   }
+}
 }
