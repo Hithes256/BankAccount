ebdd37d949e957e73987baf592424a714cb16385
Hithes256 <hitheshkumar246@gmail.com>
2026-01-29 09:32:37 +0530

    Update BankService.java

--- BankService.java
+++ BankService.java
@@ -33,15 +33,15 @@ public class BankService {
     // The main entry point
     public static void main(String[] args) {
         // Create an instance of the BankAccount class
-        BankAccount acc = new BankAccount(8000);
+        BankAccount acc = new BankAccount(9000);
         
         System.out.println("Initial Balance: " + acc.getBalance());
 
         // Perform operations
-        acc.deposit(800);
+        acc.deposit(500);
         System.out.println("Balance after deposit of 500: " + acc.getBalance());
 
-        acc.withdraw(800);
+        acc.withdraw(500);
         System.out.println("Balance after withdrawal of 100: " + acc.getBalance());
 
         // Print final result
