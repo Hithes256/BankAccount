a1a822f62a96ec080e4ed9165db7b2a6e695f622
Hithes256 <hitheshkumar246@gmail.com>
2026-01-29 16:55:25 +0530

    Create StringLength.java

---
+++ StringLength.java
@@ -0,0 +1,7 @@
+public class StringLength{
+  public static void main(String[] args){
+    String str="Hithesh Kumar";
+    int length=str.length();
+    System.out.println("Length of string "+str+" is "+length);
+  }
+}
