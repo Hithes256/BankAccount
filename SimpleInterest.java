953cd2c325f02f5cb3d0d58eb5569e6aa17bbbda
Hithes256 <hitheshkumar246@gmail.com>
2026-01-29 16:42:17 +0530

    Update SimpleInterest.java

--- SimpleInterest.java
+++ SimpleInterest.java
@@ -1,9 +1,11 @@
 public class SimpleInterest{
   public static void main(String[] args){
-    int principal=100;
+    int principal=1000;
     int rate=5;
     int time=2;
     int SI=(principal * rate * time)/100;
     System.out.println("Simple interest is "+SI);
+    int totalAmt=principal + SI;
+    System.out.println("Total Amount is "+totalAmt);
   }
 }
