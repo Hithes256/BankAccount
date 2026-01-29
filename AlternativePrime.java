3c7c412d5b8533816b8ba82e8b220844928f5240
Hithes256 <hitheshkumar246@gmail.com>
2026-01-29 17:52:32 +0530

    Create AlternativePrime.java

---
+++ AlternativePrime.java
@@ -0,0 +1,20 @@
+public class AlternativePrime{
+  public static void main(String[] args){
+    int n=50,count=0;
+    for(int num=2;num<=n;num++){
+      boolean isPrime=true;
+      for(int i=2;i<num;i++){
+        if(num%i==0){
+          isPrime=false;
+          break;
+        }
+        if(isPrime){
+          count++;
+          if(count%2==1){
+            System.out.println(num+" ");
+          }
+        }
+      }
+    }
+  }
+}
