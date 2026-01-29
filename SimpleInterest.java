67806606f12719c8ae00d86b3d9397065aa707ee
Hithes256 <hitheshkumar246@gmail.com>
2026-01-29 16:37:57 +0530

    Add SimpleInterest class to calculate interest

---
+++ SimpleInterest.java
@@ -0,0 +1,9 @@
+public class SimpleInterest{
+  public static void main(String[] args){
+    int principal=100;
+    int rate=5;
+    int time=2;
+    int SI=(principal * rate * time)/100;
+    System.out.println("Simple interest is "+SI);
+  }
+}
