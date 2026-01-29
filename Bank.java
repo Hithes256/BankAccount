dac441b44d92d2eba24bc7d2e8a14beef0c13e4f
Hithes256 <hitheshkumar246@gmail.com>
2026-01-29 17:22:11 +0530

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -1,8 +1,8 @@
 public class Bank{
   public static void main(String[] args){
     double balance=1000;
-    double depositAmt=500;
-    double withdrawAmt=300;
+    double depositAmt=800;
+    double withdrawAmt=600;
 
     System.out.println("Initial Balance: "+balance);
 
